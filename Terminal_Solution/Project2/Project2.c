e5a34c9f98f3005d6106c7e168d8bc964a4b3571
sohee <totns6543@naver.com>
2020-08-04 23:01:44 +0900

    터미널 프로젝트

---
+++ Terminal_Solution/Project2/Project2.c
@@ -0,0 +1,89 @@
+#pragma warning ( disable : 4996 )
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+#include <Windows.h>
+
+struct _finddata_t fd;
+
+int isFileOrDir()
+{
+    if (fd.attrib & _A_SUBDIR)
+        return 0; // 디렉토리면 0 반환
+    else
+        return 1; // 그밖의 경우는 "존재하는 파일"이기에 1 반환
+
+}
+
+void FileSearch(char file_path[])
+{
+    intptr_t handle;
+    int check = 0;
+    char file_path2[_MAX_PATH];
+
+    strcat(file_path, "\\");
+    strcpy(file_path2, file_path);
+    strcat(file_path, "*");
+
+    if ((handle = _findfirst(file_path, &fd)) == -1)
+    {
+        printf("No such file or directory\n");
+        return;
+    }
+
+    while (_findnext(handle, &fd) == 0)
+    {
+        char file_pt[_MAX_PATH];
+        strcpy(file_pt, file_path2);
+        strcat(file_pt, fd.name);
+
+        check = isFileOrDir();    //파일인지 디렉토리 인지 식별
+
+        if (check == 0 && fd.name[0] != '.')
+        {
+            printf("폴더명 : %s\n", file_pt);    //파일명 호출
+        }
+        else if (check == 1 && fd.size != 0 && fd.name[0] != '.')
+        {
+            printf("파일명 : %s\n", file_pt);
+        }
+        
+    }
+    _findclose(handle);
+}
+
+int main()
+{
+    char command[10];
+    char path[100] = { 'a' };
+    
+    while (1)
+    {
+        scanf("%s", &command);
+
+        if (strcmp(command, "ls") == 0)
+        {
+            char file_path[_MAX_PATH] = "C:\\dev\\Mini_Terminal\\Terminal_Solution";
+            FileSearch(file_path);
+        }
+
+        else
+        {
+            scanf("%s", &path);
+        }
+
+
+        if (strcmp(command, "cd") == 0)
+        {
+            char file_path[_MAX_PATH] = { 'a' };
+            strcpy(file_path, path);
+            printf("\n file_path : %s \n path : %s\n", file_path, path);
+            FileSearch(file_path);
+        }
+    }
+      
+    return 0;
+
+}
+
