c828310f42833b4a731a38a405a2a1a03dbf8787
sohee <totns6543@naver.com>
2020-08-30 17:18:53 +0900

    .lib 파일로 나누는거 연습

---
+++ Terminal_Solution/StaticMath/TerminalLibrary/CdCheck.c
@@ -0,0 +1,41 @@
+#include "terminal.h"
+
+extern char path[100];
+extern char file_path[_MAX_PATH];
+extern char upperDir[100];
+extern int check;
+extern int len;
+
+void CdCheck()
+{
+    scanf("%s", &path);
+
+    if (strcmp(path, "..") == 0)
+    {
+        char upperDir[100] = "";
+        char* ptr = strrchr(file_path, '\\');
+        len = strlen(file_path) - strlen(ptr);
+        for (int i = 0; i < len; i++)
+        {
+            upperDir[i] = file_path[i];
+        }
+        strcpy(file_path, upperDir);
+        printf("\n");
+    }
+    else
+    {
+        strcat(file_path, "\\");
+        strcat(file_path, path);
+
+        check = FileCheck(file_path);
+        if (check == 3)
+        {
+            RmBackslash();
+            printf("\n");
+        }
+        else
+        {
+            printf("\n");
+        }
+    }
+}
