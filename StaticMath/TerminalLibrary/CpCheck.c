9ca3795c4833a94ea6510b796a12017ab8c6e8a4
sohee <totns6543@naver.com>
2020-08-30 17:23:37 +0900

    lib로 파일 나누기

---
+++ StaticMath/TerminalLibrary/CpCheck.c
@@ -0,0 +1,101 @@
+#pragma once
+#include "terminal.h"
+
+
+extern FILE* sfp;
+extern FILE* dfp;
+extern char source[_MAX_PATH], dest[_MAX_PATH];
+extern char copy;
+extern char path2[_MAX_PATH];
+extern char file_path[_MAX_PATH];
+extern char pathName[20];
+extern char copyPath[_MAX_PATH];
+extern int check;
+extern int check2;
+extern int len;
+
+void CpCheck()
+{
+    scanf("%s", &pathName);
+
+    strcpy(path2, file_path);
+    strcat(file_path, "\\");
+    strcat(file_path, pathName);
+
+    check2 = FileCheck(file_path);
+
+    if (check2 == 3)
+    {
+        strcpy(file_path, path2);
+        check2 = FileCheck(pathName);
+
+        if (check2 == 3)
+        {
+            printf("\n");
+        }
+        else
+        {
+            scanf("%s", &copyPath);
+            char originDir[100] = "";
+            char* ptr = strrchr(copyPath, '\\');
+            len = strlen(copyPath) - strlen(ptr);
+            for (int i = 0; i < len; i++)
+            {
+                originDir[i] = copyPath[i];
+            }
+
+            check2 = FileCheck(originDir);
+            if (check2 == 3)
+            {
+                printf("\n");
+            }
+            else
+            {
+                sfp = fopen(pathName, "r");
+                dfp = fopen(copyPath, "w");
+
+                while (!feof(sfp))
+                {
+                    copy = fgetc(sfp);
+                    fputc(copy, dfp);
+                }
+
+                fclose(sfp);
+                fclose(dfp);
+            }
+        }
+    }
+    else
+    {
+        scanf("%s", &copyPath);
+        char originDir[100] = "";
+        char* ptr = strrchr(copyPath, '\\');
+        len = strlen(copyPath) - strlen(ptr);
+        for (int i = 0; i < len; i++)
+        {
+            originDir[i] = copyPath[i];
+        }
+
+        check2 = FileCheck(originDir);
+        if (check2 == 3)
+        {
+            printf("\n");
+        }
+        else
+        {
+            sfp = fopen(file_path, "r");
+            dfp = fopen(copyPath, "w");
+
+            while (!feof(sfp))
+            {
+                copy = fgetc(sfp);
+                fputc(copy, dfp);
+            }
+
+            fclose(sfp);
+            fclose(dfp);
+        }
+        strcpy(file_path, path2);
+    }
+    printf("\n");
+}
---
+++ StaticMath/TerminalLibrary/RmCheck.c
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "terminal.h"
+
+char path[100];
+extern char file_path[_MAX_PATH];
+extern int check;
+extern int len;
+
+void RmBackslash()
+{
+    char originDir[100] = "";
+    char* ptr = strrchr(file_path, '\\');
+    len = strlen(file_path) - strlen(ptr);
+    for (int i = 0; i < len; i++)
+    {
+        originDir[i] = file_path[i];
+    }
+    strcpy(file_path, originDir);
+}
+
+void RmFuntion()
+{
+    scanf("%s", &path);
+
+    strcat(file_path, "\\");
+    strcat(file_path, path);
+
+    check = FileCheck(file_path);
+    if (check == 3) //존재하지 않은 파일일 경우 잘못 된 경로 삭제
+    {
+        RmBackslash();
+        printf("\n");
+    }
+    else // 존재하는 폴더일 경우 파일 검사시작
+    {
+        intptr_t handle;
+        int check = 0;
+        char file_path2[_MAX_PATH];
+
+        strcat(file_path, "\\");
+        strcpy(file_path2, file_path);
+        strcat(file_path, "*");
+
+        if ((handle = _findfirst(file_path, &fd)) == -1)
+        {
+            return 3;
+        }
+
+        while (_findnext(handle, &fd) == 0)
+        {
+            char file_pt[_MAX_PATH];
+            strcpy(file_pt, file_path2);
+
+            check = isFileOrDir();    //파일인지 디렉토리 인지 식별
+
+            if (check == 0 && fd.name[0] != '.')
+            {
+                //폴더명 호출
+                printf("%s\n", fd.name);
+            }
+            else if (check == 1 && fd.size != 0 && fd.name[0] != '.')
+            {
+                //파일명 호출
+
+                RmBackslash();
+                strcat(file_path, "\\");
+                strcat(file_path, fd.name);
+
+                //char strPath = file_path;
+                int Result = remove(file_path);
+
+                if (Result == 0)
+                {
+                    printf("\n");
+                }
+                else if (Result == -1)
+                {
+                    printf("\n");
+                }
+
+            }
+        }
+        _findclose(handle);
+    }
+    RmBackslash();
+
+    int pResult = _rmdir(file_path);
+
+    if (pResult == 0)
+    {
+        RmBackslash();
+    }
+    else if (pResult == -1)
+    {
+        printf("\n");
+    }
+}
